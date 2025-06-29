dd9db00e420df3cb67781ab78586214effde080e
jesse <jesselouiselat@gmail.com>
2025-06-30 00:36:49 +0800

    FE env update

--- frontend_islatambay/src/api/AxiosInstance.js
+++ frontend_islatambay/src/api/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://islatambay.onrender.com/",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
