f1c0b8d7352bb00ee8a9fd5b7fbb976f138e4918
jesse <jesselouiselat@gmail.com>
2025-06-30 01:30:04 +0800

    fixed local annd live middleware

--- frontend_islatambay/vite.config.js
+++ frontend_islatambay/vite.config.js
@@ -1,15 +1,12 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "https://islatambay.onrender.com/",
-        changeOrigin: true,
-      },
+      "/api": "http://localhost:5000",
     },
   },
 });
