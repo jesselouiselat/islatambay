55321a2e9f51f928ccd8560a14d259c5d009f5c6
jesse <jesselouiselat@gmail.com>
2025-07-01 15:43:10 +0800

    fixed googleauth 3

--- frontend_islatambay/src/GoogleLogin.jsx
+++ frontend_islatambay/src/GoogleLogin.jsx
@@ -1,7 +1,7 @@
 function GoogleLogin() {
   async function handleClick(event) {
     try {
-      window.location.href = `${import.meta.env.VITE_API_URL}api/auth/google`;
+      window.location.href = `${import.meta.env.VITE_API_URL}/api/auth/google`;
     } catch (error) {
       alert(error.response.status);
     }
